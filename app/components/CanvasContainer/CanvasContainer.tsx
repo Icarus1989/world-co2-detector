5d0e0c69c5a31fd4d621ee4c2cb9621e97bb2a0e
Icarus1989 <53438706+Icarus1989@users.noreply.github.com>
2025-09-10 11:44:08 +0200

    small clean

--- app/components/CanvasContainer/CanvasContainer.tsx
+++ app/components/CanvasContainer/CanvasContainer.tsx
@@ -10,23 +10,16 @@ import styles from "./CanvasContainer.module.css";
 // import type { ContinentInfo } from "@/app/utilities/types/types";
 
 export function CanvasContainer({
-	// analyzeCoastLineData,
-	// analyzeCoastLandData,
 	analyzeLandsData,
 	analyzeBoundaryLines,
 	analyzeInternalBound,
-	// analyzeRiversData,
 	analyzeBreakLines
 }: // loadEXR
 {
-	// analyzeCoastLineData: () => Promise<ContinentInfo[]>;
-	// analyzeCoastLandData: () => Promise<any>;
 	analyzeLandsData: () => Promise<any>;
 	analyzeBoundaryLines: () => Promise<any>;
 	analyzeInternalBound: () => Promise<any>;
-	// analyzeRiversData: () => Promise<any>;
 	analyzeBreakLines: () => Promise<any>;
-	// loadEXR: () => Promise<THREE.TextureJSON>;
 }) {
 	return (
 		<div className={styles["container"]}>
@@ -35,12 +28,9 @@ export function CanvasContainer({
 			<TextInputTemplate fieldName="coords" labels={["lat", "lon"]} />
 			<DateInputComponent /> */}
 			<CanvasElement
-				// analyzeCoastLineData={analyzeCoastLineData}
-				// analyzeCoastLandData={analyzeCoastLandData}
 				analyzeLandsData={analyzeLandsData}
 				analyzeBoundaryLines={analyzeBoundaryLines}
 				analyzeInternalBound={analyzeInternalBound}
-				// analyzeRiversData={analyzeRiversData}
 				analyzeBreakLines={analyzeBreakLines}
 
 				// loadEXR={loadEXR}
